32af6a95d98943959c2cb3fb6519f672c69c9716
Matias Aduco <matias.aduco@gmail.com>
2025-08-27 21:31:03 -0300

    Added PupilKeyboard into CustomEditor

--- src/providers/PupilEditorProvider.ts
+++ src/providers/PupilEditorProvider.ts
@@ -1,7 +1,5 @@
 import * as vscode from 'vscode';
 import { getWebviewContent } from '../utils/getWebviewContent.js';
-import { sendTheme } from '../events/sendTheme.js';
-import { openText } from '../events/openText.js';
 
 export class PupilEditorProvider implements vscode.CustomTextEditorProvider {
 	constructor(private readonly context: vscode.ExtensionContext) {}
@@ -36,34 +34,42 @@ export class PupilEditorProvider implements vscode.CustomTextEditorProvider {
 		webviewPanel.webview.html = getWebviewContent(this.context, webviewPanel.webview);
 
 		const eventsListener = vscode.window.onDidChangeActiveColorTheme(() => {
-			sendTheme(webviewPanel);
-		});
-
-		const openTextDocListener = vscode.workspace.onDidOpenTextDocument(() => {
-			openText(webviewPanel, document);
+			this.updateTheme(webviewPanel);
 		});
 
 		const onDidReceiveMessageListener = webviewPanel.webview.onDidReceiveMessage((message) => {
 			if (message.type === 'ready' && !webviewReady) {
 				webviewReady = true;
-				openText(webviewPanel, document);
+				this.openTextDocument(webviewPanel, document);
 			}
 			if (message.type === 'edit') {
 				this.updateTextDocument(document, message.content);
 			}
 		});
 
-		// const changeTextDocListener = vscode.workspace.onDidChangeTextDocument((e) => {
-		// 	if (e.document.uri.toString() === document.uri.toString()) {
-		// 		openText(webviewPanel, e.document);
-		// 	}
-		// });
+		const changeTextDocListener = vscode.workspace.onDidChangeTextDocument((e) => {
+			if (e.document.uri.toString() === document.uri.toString()) {
+				this.openTextDocument(webviewPanel, e.document);
+			}
+		});
 
 		webviewPanel.onDidDispose(() => {
 			eventsListener.dispose();
-			openTextDocListener.dispose();
 			onDidReceiveMessageListener.dispose();
-			// changeTextDocListener.dispose();
+			changeTextDocListener.dispose();
+		});
+	}
+
+	private openTextDocument(webviewPanel: vscode.WebviewPanel, document: vscode.TextDocument) {
+		const content = document.getText();
+		const fileName = document.fileName;
+		const fileExtension = fileName.split('.').pop() || '';
+
+		webviewPanel.webview.postMessage({
+			type: 'init',
+			content,
+			fileName,
+			fileExtension
 		});
 	}
 
@@ -75,5 +81,24 @@ export class PupilEditorProvider implements vscode.CustomTextEditorProvider {
 		);
 		edit.replace(document.uri, fullRange, value);
 		return vscode.workspace.applyEdit(edit);
+	}
+
+	private updateTheme(webviewPanel: vscode.WebviewPanel) {
+		const themeKind = vscode.window.activeColorTheme.kind;
+		let theme: string;
+	
+		switch (themeKind) {
+			case vscode.ColorThemeKind.Dark:
+				theme = 'vs-dark';
+				break;
+			case vscode.ColorThemeKind.HighContrast:
+				theme = 'hc-black';
+				break;
+			default:
+				theme = 'vs';
+				break;
+		}
+		
+		webviewPanel.webview.postMessage({ type: 'set-theme', theme });
 	}
 }
