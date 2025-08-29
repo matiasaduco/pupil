4c7d36a2ba50d6f7cdc0497bf2e3cb3b3d8619d8
Matías Aduco <73493269+matiasaduco@users.noreply.github.com>
2025-08-28 22:45:15 -0300

    Added PupilKeyboard into CustomEditor (#4)

--- src/utils/handleRegisterEvents.ts
+++
@@ -1,6 +0,0 @@
-import { sendTheme } from "../events/theme.js";
-import * as vscode from 'vscode';
-
-export function handleRegisterEvents(webviewPanel: vscode.WebviewPanel) {
-    sendTheme(webviewPanel);
-}
