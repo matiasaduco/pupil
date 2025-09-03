684ad65d85ee7f8cb5b7d824d1b47f6cbe2314aa
Matias Aduco <matias.aduco@gmail.com>
2025-09-03 20:40:38 -0300

    Arreglado Eslint

--- src/webview/contexts/VsCodeApiContext.tsx
+++ src/webview/contexts/VsCodeApiContext.tsx
@@ -1,20 +1,17 @@
-import React, { createContext, useContext } from 'react';
+import React, { createContext, useContext } from 'react'
+import { VsCodeApi } from '../../global.js'
 
-const VsCodeApiContext = createContext<any>(null);
+const VsCodeApiContext = createContext<VsCodeApi | null>(null)
 
 export const VsCodeApiProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
-  const vscode = acquireVsCodeApi();
-  return (
-    <VsCodeApiContext.Provider value={vscode}>
-      {children}
-    </VsCodeApiContext.Provider>
-  );
-};
+	const vscode = acquireVsCodeApi()
+	return <VsCodeApiContext.Provider value={vscode}>{children}</VsCodeApiContext.Provider>
+}
 
 export const useVsCodeApi = () => {
-  const context = useContext(VsCodeApiContext);
-  if (!context) {
-    throw new Error('useVsCodeApi must be used within a VsCodeApiProvider');
-  }
-  return context;
-};
+	const context = useContext(VsCodeApiContext)
+	if (!context) {
+		throw new Error('useVsCodeApi must be used within a VsCodeApiProvider')
+	}
+	return context
+}
