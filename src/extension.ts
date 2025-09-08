bd499a657ec8353931908a2d448ca6626dedb489
Matías Aduco <73493269+matiasaduco@users.noreply.github.com>
2025-09-07 23:43:00 -0300

    Eliminadas las referencias al Eyetracker (#21)

--- src/extension.ts
+++ src/extension.ts
@@ -1,21 +1,9 @@
 import * as vscode from 'vscode'
 import { PupilEditorProvider } from './providers/PupilEditorProvider.js'
-import { EyetrackerProvider } from './providers/EyetrackerProvider.js'
-
-let eyetrackerWebview: EyetrackerProvider | undefined
 
 export function activate(context: vscode.ExtensionContext) {
 	const editor = PupilEditorProvider.register(context)
 	context.subscriptions.push(editor)
-
-	// Registrar el comando y el WebSocket server
-	eyetrackerWebview = new EyetrackerProvider()
-	const disposable = eyetrackerWebview.register()
-	context.subscriptions.push(disposable)
 }
 
-export function deactivate() {
-	if (eyetrackerWebview) {
-		eyetrackerWebview.dispose()
-	}
-}
+export function deactivate() {}
