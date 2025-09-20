3c589cbccad23106db082d37009b94c801434a8b
gonzabender <85466193+gonzabender@users.noreply.github.com>
2025-09-20 08:58:19 -0300

    Gbd shortcuts (#35)
    * Separado el boton del dialog
    
    * Fix estilo
    
    * Fix

--- src/webview/components/PupilKeyboard/CreateButton.tsx
+++ src/webview/components/PupilKeyboard/CreateButton.tsx
@@ -1,74 +1,20 @@
-import { useState, useEffect } from 'react'
-import { Button, Dialog, DialogTitle, DialogContent, TextField, DialogActions, RadioGroup, FormControlLabel, Radio} from '@mui/material'
-import { useVsCodeApi } from '@webview/contexts/VsCodeApiContext.js'
+import { useState } from 'react'
+import { Button } from '@mui/material'
+import CreateDialog from './CreateDialog.js'
 
 const CreateButton = () => {
-  const vscode = useVsCodeApi()
   const [open, setOpen] = useState(false)
-  const [name, setName] = useState('')
-  const [type, setType] = useState<'file' | 'folder'>('file')
-  const [coords, setCoords] = useState({ x: 0, y: 0 })
-
- 
-  useEffect(() => {
-    const handleMiddleClick = (e: MouseEvent) => {
-      if (e.button === 1) {
-        e.preventDefault()
-        setCoords({ x: e.clientX, y: e.clientY })
-        setOpen(true)
-      }
-    }
-
-    window.addEventListener('mouseup', handleMiddleClick, true)
-    return () => window.removeEventListener('mouseup', handleMiddleClick, true)
-  }, [])
-
-  const handleConfirm = () => {
-    vscode.postMessage({ type: `create-${type}`, name })
-    setOpen(false)
-    setName('')
-    setType('file')
-  }
 
   return (
     <>
-      <Dialog
-        open={open}
-        onClose={() => setOpen(false)}
-        PaperProps={{
-          sx: {
-            position: 'absolute',
-            top: coords.y,
-            left: coords.x,
-            transform: 'translate(-50%, -50%)', // center on cursor
-            backgroundColor: '#2b2b2b',
-            color: '#fff',
-            pointerEvents: 'auto'
-          }
-        }}
-      >
-        <DialogTitle>Create New {type === 'file' ? 'File' : 'Folder'}</DialogTitle>
-        <DialogContent>
-          <RadioGroup row value={type} onChange={(e) => setType(e.target.value as 'file' | 'folder')} sx={{ mb: 2 }}>
-            <FormControlLabel value="file" control={<Radio sx={{ color: '#fff' }} />} label="File" sx={{ color: '#fff' }}/>
-            <FormControlLabel value="folder" control={<Radio sx={{ color: '#fff' }} />} label="Folder" sx={{ color: '#fff' }}/>
-          </RadioGroup>
+      <Button  onClick={() => setOpen(true)}>
+        New File/Folder
+      </Button>
 
-          <TextField autoFocus margin="dense" label={`${type === 'file' ? 'File' : 'Folder'} name`} fullWidth variant="outlined" value={name}
-            onChange={(e) => setName(e.target.value)}
-            InputProps={{ sx: { color: '#fff' } }}
-            InputLabelProps={{ sx: { color: '#aaa' } }}
-          />
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={() => setOpen(false)} sx={{ color: '#ccc' }}>
-            Cancel
-          </Button>
-          <Button  onClick={handleConfirm} disabled={!name.trim()} sx={{ color: '#fff' }}>
-            Create
-          </Button>
-        </DialogActions>
-      </Dialog>
+      <CreateDialog
+        externalOpen={open}
+        onExternalClose={() => setOpen(false)}
+      />
     </>
   )
 }
---
+++ src/webview/components/PupilKeyboard/CreateDialog.tsx
@@ -0,0 +1,77 @@
+import { useState, useEffect } from 'react'
+import { RadioGroup, FormControlLabel, Radio, TextField } from '@mui/material'
+import { useVsCodeApi } from '@webview/contexts/VsCodeApiContext.js'
+import PupilDialog from '../PupilDialog/PupilDialog.js'
+
+type CreateDialogProps = {
+  externalOpen?: boolean
+  onExternalClose?: () => void
+}
+
+const CreateDialog = ({ externalOpen, onExternalClose }: CreateDialogProps) => {
+  const vscode = useVsCodeApi()
+  const [open, setOpen] = useState(false)
+  const [name, setName] = useState('')
+  const [type, setType] = useState<'file' | 'folder'>('file')
+  
+
+  useEffect(() => {
+    const handleMiddleClick = (e: MouseEvent) => {
+      if (e.button === 1) {
+        e.preventDefault()
+        setOpen(true)
+      }
+    }
+    window.addEventListener('mouseup', handleMiddleClick, true)
+    return () => window.removeEventListener('mouseup', handleMiddleClick, true)
+  }, [])
+
+  
+  useEffect(() => {
+    if (externalOpen) {
+      setOpen(true)
+    }
+  }, [externalOpen])
+
+  const reset = () => {
+    setOpen(false)
+    setName('')
+    setType('file')
+    onExternalClose?.()
+  }
+
+  const handleConfirm = () => {
+    vscode.postMessage({ type: `create-${type}`, name })
+    reset()
+  }
+
+  return (
+    <PupilDialog
+      open={open}
+      title={`Create New ${type === 'file' ? 'File' : 'Folder'}`}
+      onSubmit={handleConfirm}
+      onCancel={reset}
+      onClose={reset}
+    >
+        <RadioGroup
+          row
+          value={type}
+          onChange={(e) => setType(e.target.value as 'file' | 'folder')}>
+          <FormControlLabel value="file" control={<Radio />} label="File" />
+          <FormControlLabel value="folder" control={<Radio />} label="Folder" />
+        </RadioGroup>
+
+        <TextField
+          autoFocus
+          label={`${type === 'file' ? 'File' : 'Folder'} name`}
+          fullWidth
+          variant="outlined"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+      
+    </PupilDialog>
+  )
+}
+
+export default CreateDialog
