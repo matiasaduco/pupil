33173bcca79571e311b912f16c763e2ae987b29a
gonzabender <gonzabender@gmail.com>
2025-10-03 17:49:36 -0300

    Fix error merge

--- src/webview/App.tsx
+++ src/webview/App.tsx
@@ -14,7 +14,7 @@ const App = () => {
 			<MockPupilEditorProvider>
 				<VsCodeApiProvider>
 					<KeyboardFocusProvider>
-						<PupilContainer visible />
+						<PupilContainer />
 						<FolderTreeContainer />
 					</KeyboardFocusProvider>
 				</VsCodeApiProvider>
