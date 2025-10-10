7745adec754c079c15429f643aa31d9f7f490f64
gonzabender <gonzabender@gmail.com>
2025-10-10 17:18:19 -0300

    Agregado cambio layout con shift/caps

--- src/webview/components/PupilKeyboard/hooks/usePupilKeyboard.ts
+++ src/webview/components/PupilKeyboard/hooks/usePupilKeyboard.ts
@@ -8,16 +8,6 @@ import { useState } from 'react'
 
 const usePupilKeyboard = () => {
 	const [isShifted, setIsShifted] = useState(false)
-	// const layout = {
-	// 	shift: [
-	// 		shortcuts,
-	// 		'~ ! @ # $ % ^ & * ( ) _ + {bksp}',
-	// 		'{tab} Q W E R T Y U I O P { } |',
-	// 		'{lock} A S D F G H J K L : " {enter}',
-	// 		'{shift} Z X C V B N M < > ? {shift}',
-	// 		'.com @ {space}'
-	// 	]
-	// }
 
 	const defaultLayout: Layout = {
 		default: [
