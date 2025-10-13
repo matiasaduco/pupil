bb99d48baaf1c136948bc52ae9964ce3990a1f59
Matias Aduco <matias.aduco@gmail.com>
2025-10-13 18:22:13 -0300

    Added web for STT

--- src/extension.ts
+++ src/extension.ts
@@ -4,6 +4,18 @@ import { PupilEditorProvider } from './providers/PupilEditorProvider.js'
 export function activate(context: vscode.ExtensionContext) {
 	const editor = PupilEditorProvider.register(context)
 	context.subscriptions.push(editor)
+
+	const disposable = vscode.commands.registerCommand('pupil.openSpeechWeb', async () => {
+		const terminal = vscode.window.createTerminal('SpeechWeb Server')
+		const serverPath = context.asAbsolutePath('speech-web/server.js')
+		terminal.sendText(`node "${serverPath}"`, true)
+		terminal.show()
+
+		setTimeout(() => {
+			vscode.env.openExternal(vscode.Uri.parse('http://localhost:3000'))
+		}, 2000)
+	})
+	context.subscriptions.push(disposable)
 }
 
 export function deactivate() {}
