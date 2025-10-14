9b1e4e543338e197f2b928796262e64d161a297d
Matias Aduco <matias.aduco@gmail.com>
2025-10-13 22:00:19 -0300

    Added WebSocket for Transcript

--- src/extension.ts
+++ src/extension.ts
@@ -1,7 +1,10 @@
 import * as vscode from 'vscode'
 import { PupilEditorProvider } from './providers/PupilEditorProvider.js'
+import { WebSocketServer } from 'ws'
 
 export function activate(context: vscode.ExtensionContext) {
+	let wss: WebSocketServer | undefined
+
 	const editor = PupilEditorProvider.register(context)
 	context.subscriptions.push(editor)
 
@@ -14,7 +17,31 @@ export function activate(context: vscode.ExtensionContext) {
 		setTimeout(() => {
 			vscode.env.openExternal(vscode.Uri.parse('http://localhost:3000'))
 		}, 2000)
+
+		wss = new WebSocketServer({ port: 8080 })
+
+		wss.on('connection', (ws) => {
+			console.log('Client connected')
+
+			ws.on('message', (message) => {
+				const transcript = message.toString()
+				console.log(`Received message: ${transcript}`)
+				vscode.window.showInformationMessage(`Transcripción: ${transcript}`)
+			})
+
+			ws.on('close', () => {
+				console.log('Client disconnected')
+			})
+		})
 	})
+
+	context.subscriptions.push({
+		dispose: () => {
+			wss?.close()
+			wss = undefined
+		}
+	})
+
 	context.subscriptions.push(disposable)
 }
 
